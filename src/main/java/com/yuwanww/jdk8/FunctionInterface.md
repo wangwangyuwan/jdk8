fb7316759b71203bca5abd80fff2674736104b74
wangwangyuwan <muyouyuwano@163.com>
2019-07-26 18:11:45 +0800

    initialization files

---
+++ src/main/java/com/yuwanww/jdk8/FunctionInterface.md
@@ -0,0 +1,8 @@
+```
+(param1,param2)->{
+}
+```
+**函数式接口**
+* 只声明一个抽象方法
+* 声明了 _@FunctionalInterface_ 注解
+* 满足函数式接口条件，但是没有声明 _@FunctionalInterface_ 注解，编译器依然把该接口判定为函数式接口
---
+++ src/main/java/com/yuwanww/jdk8/ThreadDemo.java
@@ -0,0 +1,7 @@
+package com.yuwanww.jdk8;
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 3, 4, 5, 5, 5};
+    }
+}
